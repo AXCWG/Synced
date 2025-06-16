d3bc57b51dfadb07a79b1817883cf9cd2abf49d0
AXCWG <xiey0@qq.com>
2025-06-16 20:43:21 +0800

    Avalonia it is.

---
+++ Synced.Client/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Synced.Client
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
